60dcf93c426e31c14b55ffc5e2ead8b7b3753958
des.tiny17 <des.tiny17@badc73c4-aa92-11dd-b9e7-8d5594d729c2>
2008-11-17 16:45:57 +0000

    Empty class definitions for most required classes - still unsure whether or not to have separate SubdivisionScheme and HalfEdgeScheme classes.

---
+++ trunk/src/part2Project/HalfEdge.java
@@ -0,0 +1,6 @@
+package part2Project;
+
+public class HalfEdge
+{
+
+}
---
+++ trunk/src/part2Project/Vertex.java
@@ -0,0 +1,20 @@
+package part2Project;
+
+public class Vertex
+{
+	private double x,y,z;
+	
+	public Vertex(double x, double y, double z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public Vertex(Vertex v)
+	{
+		this.x = v.x;
+		this.y = v.y;
+		this.z = v.z;
+	}
+}
