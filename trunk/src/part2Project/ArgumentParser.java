7737287ec48a8a134372f911eff4d88fddb0147c
des.tiny17 <des.tiny17@badc73c4-aa92-11dd-b9e7-8d5594d729c2>
2008-11-17 17:44:41 +0000

    Added basic content to some of the simpler classes.

--- trunk/src/part2Project/ArgumentParser.java
+++ trunk/src/part2Project/ArgumentParser.java
@@ -2,5 +2,55 @@ package part2Project;
 
 public class ArgumentParser
 {
-
+	int numSteps = -1;		//Default starting values or exceptions?
+	double runTime = -1;
+	boolean printStats = false;
+	DataStructure ds = DataStructure.WINGEDEDGE;	
+	
+	enum DataStructure {WINGEDEDGE/*, QUADTREE*/};
+	
+	public ArgumentParser(String[] args)
+	{
+		for(String s : args)
+		{
+			if(s.startsWith("steps="))
+			{
+				numSteps = Integer.parseInt(s.substring(6));
+				System.out.println("Number of steps set to " + numSteps);
+			}
+			else if(s.startsWith("time="))
+			{
+				runTime = Double.parseDouble(s.substring(5));
+				System.out.println("Run time set to " + runTime);
+			}
+			else if(s.equals("stats"))
+			{
+				printStats = true;
+				System.out.println("Stats will be printed");
+			}
+			else if(s.startsWith("scheme="))
+			{
+				String scheme = s.substring(7);
+				if(scheme.equals("wingededge")) ds = DataStructure.WINGEDEDGE;
+				//else if(scheme.equals("quadtree")) ds = DataStructure.QUADTREE;
+				System.out.println("Scheme set to " + ds);
+			}
+			else if(s.equals("help"))
+			{
+				System.out.printf(helpText());
+			}
+			else System.err.println("Unrecognised argument: " + s);
+		}
+	}
+	
+	private String helpText()
+	{
+		return new String("" +
+				"steps=x\tRun the subdivision algorithm for x steps.\n" +
+				"time=t\tRun the subdivision algorithm for t seconds.\n" +
+				"stats\tPrint information about the mesh after each subdivision step.\n" +
+				"scheme=s\tUse subdivision scheme s.\n" +
+				"\tCurrent options: wingededge\n" +
+				"help\tShow this information.\n");
+	}
 }
